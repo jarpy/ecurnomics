97811407922aef2db9e9994f1dc06c2f80531229
Toby McLaughlin <toby@jarpy.net>
2011-12-12 10:06:48 +1100

    Add skeleton "items" view for item list

--- ecurnomics/views.py
+++ ecurnomics/views.py
@@ -1,1 +1,13 @@
 # Create your views here.
+
+from django.http import HttpResponse
+from django.template import Context, loader
+from django.shortcuts import render_to_response
+
+from ecurnomics.models import Auction
+
+def items(request):
+    items = Auction.objects.values('class_tsid').distinct().order_by('class_tsid')
+    template = loader.get_template('items/index.html')
+    context = Context({'items': items})
+    return HttpResponse(template.render(context))
