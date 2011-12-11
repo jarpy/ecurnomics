13b04703c26b56318fd90ba344425a0f7f7e74c8
Toby McLaughlin <toby@jarpy.net>
2011-12-12 09:58:44 +1100

    Add model for auction data

--- ecurnomics/models.py
+++ ecurnomics/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+import datetime
+
+# Create your models here.
+
+class Auction(models.Model):
+    auction_code = models.CharField(max_length=64, primary_key=True)
+    class_tsid = models.CharField(max_length=64)
+    cost = models.IntegerField()
+    category = models.CharField(max_length=64)
+    created = models.IntegerField()
+    created_milliseconds = models.BigIntegerField(max_length=16)
+    datetime_created = models.DateTimeField()
+    expires = models.IntegerField()
+    url = models.CharField(max_length=255)
+    count = models.IntegerField()
+    unit_cost = models.FloatField()
+
+    def __unicode__(self):
+        return "%s %s at %s (%.2f/unit)" % (self.count, self.class_tsid, self.cost, self.unit_cost)
