4598a1b74562db7807d61aecfac864ede846452e
Toby McLaughlin <toby@jarpy.net>
2011-12-12 10:02:29 +1100

    Add URL mappings for item views

--- urls.py
+++ urls.py
@@ -14,4 +14,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+    (r'^item/(?P<class_tsid>[^/]+)', 'ecurnomics.views.item'),
+    (r'^items/', 'ecurnomics.views.items'),
 )
