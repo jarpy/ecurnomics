67b0a35d89da706a1aea33dd90c228d365a6dd1f
Toby McLaughlin <toby@jarpy.net>
2011-12-12 09:48:35 +1100

    Database config

--- settings.py
+++ settings.py
@@ -11,11 +11,11 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'ENGINE': 'mysql',               # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'ecurnomics',            # Or path to database file if using sqlite3.
+        'USER': 'ecurnomics_user',                  # Not used with sqlite3.
+        'PASSWORD': 'ik3ge9iedaeruG3W',                  # Not used with sqlite3.
+        'HOST': 'bucket',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
