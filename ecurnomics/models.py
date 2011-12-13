96b4224fdd96aba883e4896ec9c7852eeaa32c79
Toby McLaughlin <toby@jarpy.net>
2011-12-13 13:43:24 +1100

    Use freindly names for items

--- ecurnomics/models.py
+++ ecurnomics/models.py
@@ -6,6 +6,17 @@ import datetime
 
 # Create your models here.
 
+class Item(models.Model):
+    class_tsid = models.CharField(max_length=64, primary_key=True)
+    name_single = models.CharField(max_length=64)
+    name_plural = models.CharField(max_length=64)
+    description = models.TextField()
+    base_cost = models.IntegerField()
+    category = models.CharField(max_length=64)
+    max_stack = models.IntegerField()
+    swf_url = models.URLField()
+    iconic_url = models.URLField()
+
 class Auction(models.Model):
     auction_code = models.CharField(max_length=64, primary_key=True)
     class_tsid = models.CharField(max_length=64)
@@ -22,9 +33,37 @@ class Auction(models.Model):
     count = models.IntegerField()
     unit_cost = models.FloatField()
 
+    item = models.ForeignKey(Item, db_column='class_tsid')
+
+
     def __unicode__(self):
         return "%s %s at %s (%.2f/unit)" % (self.count, self.class_tsid, self.cost, self.unit_cost)
 
     def _get_unit_cost_fixed_decimal(self):
         return "%.2f" % (self.unit_cost)
     unit_cost_fixed_decimal = property(_get_unit_cost_fixed_decimal)
+
+
+    #{
+    # : 1,
+    # : 2791,
+    # "per_page": 1,
+    # : 1,
+    # : 2791,
+    # : {
+    #     "PIF164ETGR61NVR-1323991488": {
+    #         "player": {
+    #             "tsid": "PIF164ETGR61NVR",
+    #             "name": "Holgate"
+    #     },
+    #         "created": "1323991488",
+    #         "expires": "1324077888",
+    #         "class_tsid": "rookswort",
+    #         "category": "herbalism_supplies",
+    #         : "5",
+    #         : "1600",
+    #         : "\/auctions\/PIF164ETGR61NVR\/4eea81c0\/"
+    #     }
+    #     }
+    # } 
+    #"url""cost""count""items""total""page""pages""ok"
--- ecurnomics/views.py
+++ ecurnomics/views.py
@@ -8,11 +8,12 @@ import json
 
 
 from ecurnomics.models import Auction
+from ecurnomics.models import Item
 
 def auctions(request):
-    auctions = Auction.objects.values('class_tsid').distinct().order_by('class_tsid')
+    items = Item.objects.all().order_by('name_single')
     template = loader.get_template('auctions/index.html')
-    context = Context({'auctions': auctions})
+    context = Context({'items': items})
     return HttpResponse(template.render(context))
 
 def auctions_for_item(request, class_tsid):
@@ -29,9 +30,15 @@ def auctions_for_item(request, class_tsid):
             time_price_datum = [auction.created_milliseconds, (auction.cost / auction.count)]
             price_data.append(time_price_datum)
     price_data_as_json = json.dumps(price_data)
-   
+
+    # Try to look up the fancy, human name for the item
+    # but fall back to the class_tsid if we don't have it.
+    try:
+        item_label = auctions[0].item.name_plural
+    except:
+        item_label = class_tsid
     context = Context({'auctions': auctions,
-                       'class_tsid': class_tsid,
+                       'item_label': item_label,
                        'price_data_as_json': price_data_as_json,
                        'average_cost': "%0.1f" % (average_cost),
                        'total_count': total_count,
